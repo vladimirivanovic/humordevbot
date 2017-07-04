12f1e3fb88a5d861920acb21860b1bda3b1be7e4
vladimirivanovic <vivanovic@gmail.com>
2017-07-04 16:43:43 +0200

    weather

--- web.js
+++ web.js
@@ -87,3 +87,24 @@ function sendMessage(event) {
 
   apiai.end();
 }
+
+app.post('/ai', (req, res) => {
+  if (req.body.result.action === 'vreme') {
+    let city = req.body.result.parameters['geo-city'];
+    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+process.env.WEATHER_API_KEY+'&q='+city;
+
+    request.get(restUrl, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        let json = JSON.parse(body);
+        let msg = json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
+        return res.json({
+          speech: msg,
+          displayText: msg,
+          source: 'weather'});
+      } else {
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: 'I failed to look up the city name.'}});
+      }})
+  }
