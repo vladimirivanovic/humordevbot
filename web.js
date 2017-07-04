757cc39a2ad83b4e5f62d49dcfe5d857968a4dc4
vladimirivanovic <vivanovic@gmail.com>
2017-07-04 15:27:34 +0200

    fb page token changes

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
+const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(process.env.PORT || 5000, function() {
+const server = app.listen(process.env.PORT || 5000, function () {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
 /* For Facebook Validation */
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.status(403).end();
@@ -18,11 +19,11 @@ app.get('/webhook', function(req, res) {
 });
 
 /* Handling all messenges */
-app.post('/webhook', function(req, res) {
+app.post('/webhook', function (req, res) {
   console.log(req.body);
   if (req.body.object === 'page') {
-    req.body.entry.forEach(function(entry) {
-      entry.messaging.forEach(function(event) {
+    req.body.entry.forEach(function (entry) {
+      entry.messaging.forEach(function (event) {
         if (event.message && event.message.text) {
           sendMessage(event);
         }
@@ -31,3 +32,24 @@ app.post('/webhook', function(req, res) {
     res.status(200).end();
   }
 });
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.9/me/messages',
+    qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
