e93d7cacfa4d2b03e10473afc7e7b7271355f59d
vladimirivanovic <vivanovic@gmail.com>
2017-07-04 15:48:04 +0200

    test

--- web.js
+++ web.js
@@ -43,7 +43,7 @@ function sendMessage(event) {
     method: 'POST',
     json: {
       recipient: {id: sender},
-      message: {text: text}
+      message: {text: 'vlada'}
     }
   }, function (error, response) {
     if (error) {
