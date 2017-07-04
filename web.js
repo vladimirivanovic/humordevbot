01803f270519ea483aa1c6955ec91512c959272e
wolf <vivanovic@gmail.com>
2017-07-04 14:00:20 +0200

    Update web.js

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 5000, function() {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
 /* For Facebook Validation */
-app.get('/webhook', (req, res) => {
+app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
     res.status(200).send(req.query['hub.challenge']);
   } else {
@@ -18,11 +18,11 @@ app.get('/webhook', (req, res) => {
 });
 
 /* Handling all messenges */
-app.post('/webhook', (req, res) => {
+app.post('/webhook', function(req, res) {
   console.log(req.body);
   if (req.body.object === 'page') {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
+    req.body.entry.forEach(function(entry) {
+      entry.messaging.forEach(function(event) {
         if (event.message && event.message.text) {
           sendMessage(event);
         }
